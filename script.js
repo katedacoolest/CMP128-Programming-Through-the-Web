0bba5e64b6036a27ddfe3cea956bb899970fd5e2
katedacoolest <125290119+katedacoolest@users.noreply.github.com>
2025-03-03 17:51:32 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let quotes = {"Nothing is impossible, the word itself says 'I'm possible': Audrey Hepburn", "Keep your face always toward the sunshine - and shadows will fall behind you: Walt Whitman" , "With the new day comes new strength and new thoughts: Eleanor Roosevelt "}
+
+function newQuote(){
+  let randomNumber= Math.floor(Math.random()*quotes.length);
+
+console.log(quotes[]);
+
+}
+
