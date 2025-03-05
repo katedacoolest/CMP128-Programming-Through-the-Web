eca8874216cf7a260cd6ff9b375308d3e83d33b8
katedacoolest <125290119+katedacoolest@users.noreply.github.com>
2025-03-05 17:25:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let quotes = {"Nothing is impossible, the word itself says 'I'm possible': Audrey Hepburn", "Keep your face always toward the sunshine - and shadows will fall behind you: Walt Whitman" , "With the new day comes new strength and new thoughts: Eleanor Roosevelt "}
+let quotes = ["Nothing is impossible, the word itself says 'I'm possible': Audrey Hepburn", "Keep your face always toward the sunshine - and shadows will fall behind you: Walt Whitman" , "With the new day comes new strength and new thoughts: Eleanor Roosevelt "];
 
 function newQuote(){
   let randomQuote= Math.floor(Math.random()*quotes.length);
