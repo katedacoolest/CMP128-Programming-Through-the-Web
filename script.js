226d6fbbe590417072d93c0cefea5c7a0788035b
katedacoolest <125290119+katedacoolest@users.noreply.github.com>
2025-03-05 18:56:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//quotes
 let quotes = [
   "Nothing is impossible, the word itself says 'I'm possible' -Audrey Hepburn",
   "Keep your face always toward the sunshine - and shadows will fall behind you -Walt Whitman",
@@ -10,3 +11,21 @@ function newQuote() {
   let quoteDivElement = document.getElementById("myQuote");  
   quoteDivElement.innerHTML = myQuote;  
 }
+
+
+//messages
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const msg = document.getElementById("msg").value;
+
+   
+    if (name && email && msg) {
+        alert("Thank you for your message, " + name + "!");
+    } else {
+        alert("Please fill in all fields.");
+    }
+    document.getElementById("contactForm").reset();
+});
